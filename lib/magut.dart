83d1f9ae59c2ea28a203d229740061a1aff8c248
dbbd59 <davide.bolzoni59@gmail.com>
2023-01-11 14:41:16 +0100

    export tokens model

--- lib/magut.dart
+++ lib/magut.dart
@@ -3,6 +3,7 @@ library magut;
 
 export 'src/auth/jwt_utils.dart';
 export 'src/auth/token_utils.dart';
+export 'src/auth/tokens.dart';
 export 'src/network/rest_client.dart';
 export 'src/storage/local_storage.dart';
 export 'src/utils/calendar_utils.dart';
